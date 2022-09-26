72c727dfaf6b6a324ab84352934cf82c42cb478e
CyberGuth <austinguth@gmail.com>
2022-09-26 14:21:15 -0500

    yup

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ $('.radio-buttons input').click(function(){
   $('#image-frame img').attr('src', this.value);
 
 })
+
+$('.radio-buttons2 input').click(function(){
+
+  // // stuff that happens when I click on an input
+
+  $('#image-frame2 img').attr('src', this.value);
+
+})
